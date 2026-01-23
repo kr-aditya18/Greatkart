d23d8760e08f441ef774075c1622a1792dc28e6a
kr-aditya18 <vermaadiitya13678@gmail.com>
2026-01-23 19:42:37 +0530

    Fix ROOT_URLCONF and finalize settings

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -3,13 +3,22 @@ import os
 from dotenv import load_dotenv
 import dj_database_url
 
+# --------------------------------------------------
+# BASE
+# --------------------------------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
+# --------------------------------------------------
+# SECURITY
+# --------------------------------------------------
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
+# --------------------------------------------------
+# APPLICATIONS
+# --------------------------------------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -18,9 +27,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party
     'cloudinary',
     'cloudinary_storage',
+    'admin_thumbnails',
 
+    # Local apps
     'category',
     'accounts',
     'store',
@@ -28,26 +40,9 @@ INSTALLED_APPS = [
     'orders',
 ]
 
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-
-                'category.context_processors.menu_links',
-                'carts.context_processors.counter',
-            ],
-        },
-    },
-]
-
+# --------------------------------------------------
+# MIDDLEWARE
+# --------------------------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -59,6 +54,40 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# --------------------------------------------------
+# 🔥 THIS WAS MISSING (MAIN BUG)
+# --------------------------------------------------
+ROOT_URLCONF = 'greatkart.urls'
+
+# --------------------------------------------------
+# TEMPLATES
+# --------------------------------------------------
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'category.context_processors.menu_links',
+                'carts.context_processors.counter',
+            ],
+        },
+    },
+]
+
+# --------------------------------------------------
+# WSGI
+# --------------------------------------------------
+WSGI_APPLICATION = 'greatkart.wsgi.application'
+
+# --------------------------------------------------
+# DATABASE
+# --------------------------------------------------
 if os.getenv("DATABASE_URL"):
     DATABASES = {
         'default': dj_database_url.config(conn_max_age=600)
@@ -71,15 +100,27 @@ else:
         }
     }
 
+# --------------------------------------------------
+# AUTH
+# --------------------------------------------------
+AUTH_USER_MODEL = 'accounts.Account'
 
+# --------------------------------------------------
+# STATIC FILES
+# --------------------------------------------------
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'greatkart' / 'static']
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# --------------------------------------------------
+# MEDIA (CLOUDINARY)
+# --------------------------------------------------
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+# --------------------------------------------------
+# EMAIL
+# --------------------------------------------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -87,10 +128,14 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-AUTH_USER_MODEL = 'accounts.Account'
-
+# --------------------------------------------------
+# LOGIN
+# --------------------------------------------------
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 
+# --------------------------------------------------
+# DEFAULT PK
+# --------------------------------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
