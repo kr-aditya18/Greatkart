3ef23423201e4fdfe14289c3dc6cda8a1cf97912
kr-aditya18 <vermaadiitya13678@gmail.com>
2026-01-26 00:41:03 +0530

    Configured Brevo email via Anymail

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -160,10 +160,13 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
-
-
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 # =========================
-# EMAIL — BREVO (FINAL)
+# EMAIL — BREVO 
 # =========================
 
 EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
